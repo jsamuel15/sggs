612fbabb1b94a9c45524e271855618ea6c8193eb
João Samuel de Jesus Ferreira <samuel.ferreira@dotcoding.com.br>
2021-10-07 09:51:22 -0300

    feat(structure): criacao da lista do listPerformanceCompany

---
+++ src/ListPerformanceCompany/utils/AllListJson.ts
@@ -0,0 +1,71 @@
+const allListJson = {
+    name: 'Pedro Silva',
+    profession: 'Developer Front-end',
+    allCards: [
+        {
+            id: 1,
+            title: 'NYSE - New York Stock Exchange',
+            price: 1000,
+            valorize: true,
+            percentage: 1.5,
+        },
+        {
+            id: 2,
+            title: 'Tokyo Stock Exchange',
+            price: 800,
+            valorize: false,
+            percentage: 1.5,
+        },
+        {
+            id: 3,
+            title: 'Shanghai Stock Exchange',
+            price: 750,
+            valorize: true,
+            percentage: 2,
+        },
+        {
+            id: 4,
+            title: 'Hong Kong Stock Exchange',
+            price: 40,
+            valorize: true,
+            percentage: 6,
+        },
+        {
+            id: 5,
+            title: 'NYSE - New York Stock Exchange',
+            price: 1000,
+            valorize: true,
+            percentage: 1.5,
+        },
+        {
+            id: 6,
+            title: 'Euronext',
+            price: 9424.23,
+            valorize: false,
+            percentage: 0.23,
+        },
+        {
+            id: 7,
+            title: 'London Stock Exchange',
+            price: 324.23,
+            valorize: true,
+            percentage: 2.45,
+        },
+        {
+            id: 8,
+            title: 'Shenzhen Stock Exchange',
+            price: 2394.2,
+            valorize: false,
+            percentage: 0.83,
+        },
+        {
+            id: 9,
+            title: 'NASDAQ',
+            price: 3823.23,
+            valorize: false,
+            percentage: 0.2,
+        },
+    ],
+};
+
+export default allListJson;
