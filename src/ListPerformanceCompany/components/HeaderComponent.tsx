738cc985f1648c907da42a18944eaf731489b171
João Samuel de Jesus Ferreira <samuel.ferreira@dotcoding.com.br>
2021-10-08 08:21:57 -0300

    feat(structure): inicio do layout do header component

---
+++ src/ListPerformanceCompany/components/HeaderComponent.tsx
@@ -0,0 +1,13 @@
+// libraries
+import React from 'react';
+
+// js
+import { Container, TextHeader } from './headerStyles';
+
+const HeaderComponent: React.FC = () => (
+    <Container>
+        <TextHeader>Testando...</TextHeader>
+    </Container>
+);
+
+export default HeaderComponent;
---
+++ src/ListPerformanceCompany/components/headerStyles.ts
@@ -0,0 +1,14 @@
+// libraries
+import styled from 'styled-components';
+
+// js
+import colors from '../styles/colors';
+
+export const Container = styled.div`
+    background-color: ${colors.basic.black};
+    width: 100%;
+`;
+
+export const TextHeader = styled.div`
+    color: ${colors.basic.white};
+`;
