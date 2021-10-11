d30a4766eaeb4a00f8971e7917bb2596bf63ff73
Adler Vinicius <adler.vinicius@dotcoding.com.br>
2021-10-11 09:03:38 -0300

    fix: criacao tela home

--- src/ListPerformanceCompany/pages/Home/HomePage.tsx
+++ src/ListPerformanceCompany/pages/Home/HomePage.tsx
@@ -1,11 +1,87 @@
+/* eslint-disable consistent-return */
 // libraries
 import React from 'react';
 
-// js
-import { Container } from './homeStyles';
+// icons
+import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai';
+import {
+    Home,
+    Header,
+    Body,
+    ContainerCard,
+    DivInfoArea,
+    DivArea,
+    TextAreaName,
+    TextPriceArea,
+    ContainerDivArea,
+    TextValorize,
+    DivItemPorcent,
+} from './homeStyles';
+import allListJson from '../../utils/AllListJson';
 
-const HomePage: React.FC = () => (
-    <Container />
-);
+const HomePage: React.FC = () => {
+    const RenderItens = (type: number) => {
+        const allItemValorized = allListJson.allCards.filter((item) => item.valorize === true);
+        const allItemNotValorized = allListJson.allCards.filter((item) => item.valorize === false);
+
+        const itemNumberRender = () => {
+            if (type === 1) {
+                return allListJson.allCards;
+            }
+            if (type === 2) {
+                return allItemValorized;
+            }
+            if (type === 3) {
+                return allItemNotValorized;
+            }
+        };
+        return itemNumberRender()?.map((item) => (
+            <ContainerCard>
+                <DivInfoArea>
+                    <DivArea>
+                        <TextAreaName>{item.title}</TextAreaName>
+                    </DivArea>
+                    <ContainerDivArea>
+                        <TextPriceArea>{`$${item.price.toLocaleString('USD', { minimumFractionDigits: 2 })}`}</TextPriceArea>
+                        {
+                            item.valorize === true
+                                ? (
+                                    <TextValorize color>
+                                        <DivItemPorcent backGrounColor>
+                                            {`+${item.percentage.toFixed(2)}%`}
+                                        </DivItemPorcent>
+                                        <AiOutlineArrowUp />
+                                    </TextValorize>
+                                )
+                                : (
+                                    <TextValorize color={false}>
+                                        <DivItemPorcent backGrounColor={false}>
+                                            {`-${item.percentage.toFixed(2)}%`}
+                                        </DivItemPorcent>
+                                        <AiOutlineArrowDown />
+                                    </TextValorize>
+                                )
+                        }
+                    </ContainerDivArea>
+                </DivInfoArea>
+            </ContainerCard>
+        ));
+    };
+    // main
+    return (
+        <Home>
+            <Header />
+            <Body>
+                {RenderItens(1)}
+            </Body>
+            <Body>
+                {RenderItens(2)}
+            </Body>
+            <Body>
+                {RenderItens(3)}
+            </Body>
+        </Home>
+    );
+};
 
 export default HomePage;
