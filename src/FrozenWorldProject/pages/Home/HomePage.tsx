109e5289aab3ed3e1b88047c8afdfe625056e21e
Pedro Silva <pedro.silva@dotcoding.com.br>
2021-11-05 11:00:39 -0300

    fix(refactor): subindo codigo

--- src/FrozenWorldProject/pages/Home/HomePage.tsx
+++ src/FrozenWorldProject/pages/Home/HomePage.tsx
@@ -5,16 +5,40 @@ import img from '../../assets/images/paisagem.jpg';
 // js
 import {
     Container,
-    H1,
-    Button,
     Header,
+    H1,
+    H4,
+    H6,
+    Button,
+    ButtonTwo,
+    ButtonTree,
+    ButtonFour,
+    ButtonFive,
+    ButtonSix,
+    ButtonSeven,
+    ButtonEigth,
 } from './homeStyles';
 
+// renders
 const HomePage: React.FC = () => (
     <Container background={img}>
-        <Header> texto de tester do Header</Header>
+        <Header>
+            texto de tester do Header
+            <ButtonTree>HOME</ButtonTree>
+            <ButtonFour>PAGES</ButtonFour>
+            <ButtonFive>ABOUT</ButtonFive>
+            <ButtonSix>GALERY</ButtonSix>
+            <ButtonSeven>CONTACTS</ButtonSeven>
+            <ButtonEigth>OUR TEAM</ButtonEigth>
+        </Header>
         <H1> Feel the Fresh, Business Perspectivel </H1>
+        <H4> INNOVATIVE SOLUTIONS AND GLOBAL CONSULTING </H4>
+        <H6>
+            We elp global leaders with their organiation most critical issues and opportunities,
+            Together, we create enduring charge and results.
+        </H6>
         <Button>DETAILS</Button>
+        <ButtonTwo>CONTACT US</ButtonTwo>
     </Container>
 );
 
--- src/FrozenWorldProject/pages/Home/homeStyles.ts
+++ src/FrozenWorldProject/pages/Home/homeStyles.ts
@@ -21,11 +21,101 @@ export const Container = styled.div<Container>`
     overflow-x: auto;
 `;
 
-export const Header = styled.header`
+export const Header = styled.div`
     background-color: transparent;
     color: ${colors.basic.black};
-    height: 10vh;
-    border: solid 3px ${colors.basic.black};
+    height: 55px;
+    border: solid 1px ${colors.basic.black};
+`;
+
+export const ButtonTree = styled.button`
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    color: ${colors.basic.black};
+    margin-left: 400px;
+    margin-top: 1px;
+    width: 100px;
+    height: 45px;
+    border: solid 2px ${colors.basic.black};
+    border-radius: 15px;
+    font-size: 15px;
+    padding-left: 10px;
+`;
+
+export const ButtonFour = styled.button`
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    color: ${colors.basic.black};
+    margin-left: 400px;
+    margin-top: 1px;
+    width: 100px;
+    height: 45px;
+    border: none;
+    border-radius: 15px;
+    font-size: 15px;
+    padding-left: 10px;
+`;
+
+export const ButtonFive = styled.button`
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    color: ${colors.basic.black};
+    margin-left: 400px;
+    margin-top: 1px;
+    width: 100px;
+    height: 45px;
+    border: none;
+    border-radius: 15px;
+    font-size: 15px;
+    padding-left: 10px;
+`;
+
+export const ButtonSix = styled.button`
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    color: ${colors.basic.black};
+    margin-left: 400px;
+    margin-top: 1px;
+    width: 100px;
+    height: 45px;
+    border: none;
+    border-radius: 15px;
+    font-size: 15px;
+    padding-left: 10px;
+`;
+
+export const ButtonSeven = styled.button`
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    color: ${colors.basic.black};
+    margin-left: 400px;
+    margin-top: 1px;
+    width: 100px;
+    height: 45px;
+    border: none;
+    border-radius: 15px;
+    font-size: 15px;
+    padding-left: 10px;
+`;
+
+export const ButtonEigth = styled.button`
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    color: ${colors.basic.black};
+    margin-left: 400px;
+    margin-top: 2px;
+    width: 100px;
+    height: 45px;
+    border: none;
+    border-radius: 15px;
+    font-size: 15px;
+    padding-left: 10px;
 `;
 
 export const H1 = styled.h1`
@@ -33,9 +123,36 @@ export const H1 = styled.h1`
     padding: 250px 300px 10px 400px;
 `;
 
+export const H4 = styled.h4`
+    color: ${colors.basic.black};
+    padding: 20px 300px 10px 400px;
+    margin-top: 10px;
+`;
+
+export const H6 = styled.h6`
+    color: ${colors.basic.black};
+    padding: 20px 300px 10px 400px;
+    margin-top: 10px;
+`;
+
 export const Button = styled.button`
     background-color: ${colors.basic.black};;
     display: flex;
+    color: ${colors.basic.white};
+    margin-left: 400px;
+    margin-top: 10px;
+    width: 100px;
+    height: 45px;
+    border: solid 2px ${colors.basic.black};
+    border-radius: 15px;
+    font-size: 15px;
+    padding-left: 10px;
+`;
+
+export const ButtonTwo = styled.button`
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
     color: ${colors.basic.white};
     margin-left: 400px;
     margin-top: 10px;
