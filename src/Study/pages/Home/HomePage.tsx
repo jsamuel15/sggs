c4f5e6f6deb3dc7981ba0e3d144b2536dc2b880a
Pedro Silva <pedro.silva@dotcoding.com.br>
2022-03-28 17:32:35 -0300

    fix(refactor): subindo codigo do modulo 4 aula 3 e 4

--- src/Study/pages/Home/HomePage.tsx
+++ src/Study/pages/Home/HomePage.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-empty */
 /* eslint-disable react/react-in-jsx-scope */
 // libraries
-import React from 'react';
+import React, { useState } from 'react';
 
 // JS
 import {
@@ -10,8 +12,11 @@ import {
     Img,
     Container,
     LeftSide,
+    H1,
+    InputLeft,
+    PhraseOne,
+    Button,
     RigthSide,
-    // Button,
 } from './homeStyles';
 import {
     powered,
@@ -23,18 +28,49 @@ import {
 // import BotaoComponents from '../../components/Botao/BotaoComponents';
 // import PessoaComponents from '../../components/Pessoa/PessoaComponents';
 
-const HomePage: React.FC = () => (
-    <ContainerMain>
-        <Header>
-            <Box>
-                <Img src={powered} />
-            </Box>
-        </Header>
-        <Container>
-            <LeftSide>Lado esquerdo</LeftSide>
-            <RigthSide>Lado direito</RigthSide>
-        </Container>
-    </ContainerMain>
-);
+const HomePage: React.FC = () => {
+    const [heightField, setHeightField] = useState<number>(0);
+    const [weightField, setWeightField] = useState<number>(0);
+
+    const handleCalculateButton = () => {
+        if (heightField && weightField) {
+        } else {
+            alert('Digite todos os Campos');
+        }
+    };
+
+    return (
+        <ContainerMain>
+            <Header>
+                <Box>
+                    <Img src={powered} />
+                </Box>
+            </Header>
+            <Container>
+                <LeftSide>
+                    <H1>Calcule seu IMC.</H1>
+                    <PhraseOne>
+                        IMC é a cigla para Índice de Massa Corpórea. parâmetro adotado pela
+                        Organização Mundial da Saúde para calcular o peso ideal de cada pessoa.
+                    </PhraseOne>
+                    <InputLeft
+                        type="number"
+                        placeholder="Digite a sua Altura em metros. EX: 1.6"
+                        value={heightField > 0 ? heightField : ''}
+                        onChange={(e) => setHeightField(parseFloat(e.target.value))}
+                    />
+                    <InputLeft
+                        type="number"
+                        placeholder="Digite o seu peso. EX: 60.6"
+                        value={weightField > 0 ? weightField : ''}
+                        onChange={(e) => setWeightField(parseFloat(e.target.value))}
+                    />
+                    <Button onClick={handleCalculateButton}>Calcular</Button>
+                </LeftSide>
+                <RigthSide>Lado direito</RigthSide>
+            </Container>
+        </ContainerMain>
+    );
+};
 
 export default HomePage;
--- src/Study/pages/Home/homeStyles.ts
+++ src/Study/pages/Home/homeStyles.ts
@@ -18,7 +18,7 @@ export const Img = styled.img`
 `;
 
 export const Box = styled.div`
-    background-color: ${colors.basic.red};
+    background-color: ${colors.basic.white};
     max-width: 900px;
     margin: 40px auto;
 `;
@@ -30,9 +30,44 @@ export const Container = styled.div`
 `;
 
 export const LeftSide = styled.div`
-    background-color: ${colors.basic.red};
+    background-color: ${colors.basic.white};
     margin-right: 40px;
     flex: 1;
+`;
+
+export const H1 = styled.h1`
+    margin: 0;
+    font-size: 40px;
+    color:  ${colors.theme.grayDark};
+`;
+
+export const PhraseOne = styled.p`
+    font-size: 16px;
+    margin-bottom: 40px;
+    color: ${colors.theme.graySlate};
+`;
+
+export const InputLeft = styled.input`
+    width: 100%;
+    border: none;
+    border-bottom: 2px solid ${colors.theme.grayState};
+    padding: 10px 2px;
+    margin-bottom: 20px;
+    font-size: 14px;
+    outline: none;
+`;
+
+export const Button = styled.button`
+    background-color: ${colors.theme.blueSlate};
+    color: ${colors.basic.white};
+    font-size: 15px;
+    border: none;
+    border-radius: 10px;
+    padding: 15px 0;
+    width: 100%;
+    cursor: pointer;
+    margin-top: 40px;
+    transition: all ease .3s;
 `;
 
 export const RigthSide = styled.div`
@@ -40,8 +75,3 @@ export const RigthSide = styled.div`
     margin-left: 40px;
     flex: 1;
 `;
-
-// export const Button = styled.button`
-//     background-color: ${colors.basic.white};
-//     color: ${colors.basic.black};
-// `;
