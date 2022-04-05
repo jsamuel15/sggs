1e63ff2f1b3d61166b0a51a4ae7717f293416f04
Pedro Silva <pedro.silva@dotcoding.com.br>
2022-04-05 17:09:48 -0300

    fix(refactor): subindo codigo

--- src/Study/pages/Home/HomePage.tsx
+++ src/Study/pages/Home/HomePage.tsx
@@ -1,31 +1,14 @@
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable no-alert */
-/* eslint-disable no-empty */
-/* eslint-disable react/react-in-jsx-scope */
 // libraries
-import React, { useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 
 // JS
 import {
     ContainerMain,
-    Header,
-    Box,
-    Img,
-    Container,
-    LeftSide,
-    H1,
-    InputLeft,
-    PhraseOne,
     Button,
-    RigthBig,
-    RigthArrow,
-    ImgTwo,
-    RigthSide,
-    RigthInner,
+    Input,
+    Text,
 } from './homeStyles';
 import {
-    poweredImage,
-    leftarrowImage,
 } from '../../assets/index';
 
 // components
@@ -33,78 +16,34 @@ import {
 // import PhotoComponents from '../../components/Photo/PhotoComponents';
 // import BotaoComponents from '../../components/Botao/BotaoComponents';
 // import PessoaComponents from '../../components/Pessoa/PessoaComponents';
-import { levels, calculateImc, Level } from '../../helpers/imc';
-import GridItem from '../../components/GridItem/GridItem';
+// import { levels, calculateImc, Level } from '../../helpers/imc';
+// import GridItem from '../../components/GridItem/GridItem';
 
 const HomePage: React.FC = () => {
-    const [heightField, setHeightField] = useState<number>(0);
-    const [weightField, setWeightField] = useState<number>(0);
-    const [toShow, setToShow] = useState<Level | null>(null);
+    const [name, setName] = useState('Pedro');
+    const [lastName, setLastName] = useState('');
+    const [fullName, setFullName] = useState('');
 
-    const handleCalculateButton = () => {
-        if (heightField && weightField) {
-            setToShow(calculateImc(heightField, weightField));
-        } else {
-            alert('Digite todos os Campos');
-        }
+    useEffect(() => {
+        setFullName(`${name} ${lastName}`);
+    }, [name, lastName]);
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
     };
-
-    const handleBackButton = () => {
-        setToShow(null);
-        setHeightField(0);
-        setWeightField(0);
+    const handleLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setLastName(e.target.value);
     };
-
     return (
         <ContainerMain>
-            <Header>
-                <Box>
-                    <Img src={poweredImage} />
-                </Box>
-            </Header>
-            <Container>
-                <LeftSide>
-                    <H1>Calcule seu IMC.</H1>
-                    <PhraseOne>
-                        IMC é a cigla para Índice de Massa Corpórea. parâmetro adotado pela
-                        Organização Mundial da Saúde para calcular o peso ideal de cada pessoa.
-                    </PhraseOne>
-                    <InputLeft
-                        type="number"
-                        placeholder="Digite a sua Altura em metros. EX: 1.6"
-                        value={heightField > 0 ? heightField : ''}
-                        onChange={(e) => setHeightField(parseFloat(e.target.value))}
-                        disabled={!!toShow}
-                    />
-                    <InputLeft
-                        type="number"
-                        placeholder="Digite o seu peso. EX: 60.6"
-                        value={weightField > 0 ? weightField : ''}
-                        onChange={(e) => setWeightField(parseFloat(e.target.value))}
-                        disabled={!!toShow}
-                    />
-                    <Button onClick={handleCalculateButton} disabled={!!toShow}>Calcular</Button>
-                </LeftSide>
-                <RigthSide>
-                    {!toShow
-                    && (
-                        <RigthInner>
-                            {levels.map((item, key) => (
-                                <GridItem key={key} item={item} />
-                            ))}
-                        </RigthInner>
-                    )}
-                    {toShow
-                    && (
-                        <RigthBig>
-                            <RigthArrow onClick={handleBackButton}>
-                                <ImgTwo src={leftarrowImage} />
-                            </RigthArrow>
-                            <GridItem item={toShow} />
-                        </RigthBig>
-                    )}
-                </RigthSide>
-            </Container>
+            <Input type="text" placeholder="Digite seu nome" value={name} onChange={handleNameChange} />
+            <Input type="text" placeholder="Digite seu Sobrenome" value={lastName} onChange={handleLastNameChange} />
+            <Text>
+                Nome Completo:
+                {' '}
+                {fullName}
+            </Text>
+            <Button>Clique aqui</Button>
         </ContainerMain>
     );
 };
--- src/Study/pages/Home/homeStyles.ts
+++ src/Study/pages/Home/homeStyles.ts
@@ -6,122 +6,14 @@ import colors from '../../styles/colors';
 
 // styles
 export const ContainerMain = styled.div`
-    padding-bottom: 50px;
-    @media (max-width: 770px) {
-        padding-bottom: 50px;
-    }
-`;
-
-export const Header = styled.header`
     background-color: ${colors.basic.white};
-    @media (max-width: 770px) {
-        padding: 0 20px;
-    }
-`;
-
-export const Img = styled.img`
-    width: 150px;
-`;
-
-export const Box = styled.div`
-    background-color: ${colors.basic.white};
-    max-width: 900px;
-    margin: 40px auto;
-`;
-
-export const Container = styled.div`
-    display: flex;
-    max-width: 900px;
-    margin: auto;
-    @media (max-width: 770px) {
-        padding: 0 20px;
-        flex-direction: column;
-    }
-`;
-
-export const LeftSide = styled.div`
-    background-color: ${colors.basic.white};
-    margin-right: 40px;
-    flex: 1;
-    @media (max-width: 770px) {
-        margin-right: 0;
-    }
-`;
-
-export const H1 = styled.h1`
-    margin: 0;
-    font-size: 40px;
-    color:  ${colors.theme.grayDark};
-`;
-
-export const PhraseOne = styled.p`
-    font-size: 16px;
-    margin-bottom: 40px;
-    color: ${colors.theme.graySlate};
-`;
-
-export const InputLeft = styled.input`
-    width: 100%;
-    border: none;
-    border-bottom: 2px solid ${colors.theme.grayState};
-    padding: 10px 2px;
-    margin-bottom: 20px;
-    font-size: 14px;
-    outline: none;
 `;
 
 export const Button = styled.button`
-    background-color: ${colors.theme.blueSlate};
-    color: ${colors.basic.white};
-    font-size: 15px;
-    border: none;
-    border-radius: 10px;
-    padding: 15px 0;
-    width: 100%;
-    cursor: pointer;
-    margin-top: 40px;
-    transition: all ease .3s;
 `;
 
-export const RigthSide = styled.div`
-    background-color: ${colors.basic.blue};
-    flex: 1;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 20px;
-    @media (max-width: 770px) {
-        margin-left: 0;
-        margin-top: 50px;
-    }
+export const Input = styled.input`
 `;
 
-export const RigthBig = styled.div`
-    display: flex;
-    flex: 1;
-`;
-
-export const RigthArrow = styled.div`
-    background-color: ${colors.theme.blueSlate};
-    position: absolute;
-    display: flex;
-    width: 50px;
-    height: 50px;
-    border-radius: 50px;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    margin-left: -35px;
-    margin-top: 115px;
-    @media (max-width: 770px) {
-        margin-left: 0;
-        margin-top: 0;
-        border-radius: 10px;
-    }
-`;
-
-export const ImgTwo = styled.img`
-    width: 30px;
-`;
-
-export const RigthInner = styled.div`
+export const Text = styled.text`
 `;
